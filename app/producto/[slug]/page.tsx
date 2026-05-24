6d999650853eed24699de769dd2ea27fb1136aa9
Cristian Hernandez <cristianluis_03@live.com>
2026-05-24 01:49:22 -0500

    agregar cuarta imagen gempages

--- app/producto/[slug]/page.tsx
+++ app/producto/[slug]/page.tsx
@@ -375,6 +375,44 @@ export default function ProductoPage() {
     COMPRAR AHORA
   </button>
 </div>
+{/* CUARTA IMAGEN */}
+<Image
+  src="/sierra-bomvink-gempages-4.png"
+  alt="Sierra BOMVINK Beneficios"
+  width={1080}
+  height={3000}
+  className="w-full h-auto block"
+/>
+
+{/* BOTON 4 */}
+<div className="px-4 pb-6 pt-2 bg-black">
+  <button
+    onClick={() => setOpenCheckout(true)}
+    className="
+      w-full
+      bg-yellow-400
+      hover:bg-yellow-300
+      text-black
+      font-black
+      text-2xl
+      py-5
+      rounded-[24px]
+      shadow-[0_10px_40px_rgba(255,215,0,0.35)]
+      transition
+      active:scale-[0.98]
+      flex
+      items-center
+      justify-center
+      gap-3
+      border-b-[6px]
+      border-yellow-600
+      animate-[pulse_1.5s_ease-in-out_infinite]
+    "
+  >
+    <ShoppingCart size={28} />
+    COMPRAR AHORA
+  </button>
+</div>
       </div>
     </section>
   </>
